54bc211fe16ab5df13e9cfb02151ae282f9ebf9b
Sebastianfuent6 <90064482+sebasfuentes338@users.noreply.github.com>
2021-11-27 05:10:07 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num1=0;
+  int num2=0;
+  int resultado=0;
+  cout<<"Dijite un numero";
+  cin>>num1;
+  cout<<"Dijite un numero";
+  cin>>num2;
+  resultado=num1*num2;
+  cout<<"El producto de los numeros"<<resultado;
+resultado=num1+num2;
+cout<<"La suma de los numeros"<<resultado;
+resultado=num1-num2
+cout<<"La resta de los numeros"<<resultado
+
+
+
+
+
+
+}
